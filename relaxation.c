e50ef3d431b26a3401f86c5f9b55a9dfbc632651
Alex Remedios <alexr1993@gmail.com>
2014-11-03 18:24:58 +0000

    Get sequential algorithm working

--- relaxation.c
+++ relaxation.c
@@ -1,11 +1,32 @@
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int *read_array(char *filename, int dim)
+int dim;
+int length;
+float *arr;
+
+void print_matrix(float *arr)
+{
+    int i;
+    for (i = 0; i < length; ++i)
+    {
+        printf("%.3f ", arr[i]);
+        if (i % dim == dim - 1)
+        {
+            printf("\n");
+        }
+    }
+    printf("\n\n");
+}
+
+/* Reads square array of length dim from file. Ignores whitespace/newlines */
+float *read_array(char *filename, int dim)
 {
     int nelements = dim * dim;
-    int *arr = malloc(nelements * sizeof(int));
+    arr = malloc(nelements * sizeof(int));
     int i;
     FILE *input = fopen(filename, "r");
 
@@ -20,7 +41,7 @@ int *read_array(char *filename, int dim)
 
         // Store number
         arr[i] = temp - '0'; // Coerce char to int
-        printf("%d\n", arr[i]);
+        //printf("%.3f\n", arr[i]);
     }
 
     fclose(input);
@@ -31,43 +52,148 @@ int *read_array(char *filename, int dim)
  * Returns true if the difference between cell and it's neighbours is less
  * than prec
  */
-bool is_precise_enough (int prec, int cell, int right,
-                        int left, int above, int below )
+float calc_precision (float cell, float right, float left,
+                      float above, float below            )
 {
-    return prec > abs(cell - right) && prec > abs(cell - left)
-        && prec > abs(cell - above) && prec > abs(cell - below);
-}
+    float rightd = fabs(cell - right);
+    float leftd  = fabs(cell - left);
+    float aboved = fabs(cell - above);
+    float belowd = fabs(cell - below);
 
-void solve (int *arr, int dim, int nthreads, float precision)
-{
+    float prec_arr[4] = { rightd, leftd, aboved, belowd };
+
+    float max_precision = 0;
     int i;
-    int right, left, above, below, cell;
 
-    for (i = 0; i < dim; ++i)
+    for (i = 0; i < 4; ++i)
     {
-        cell  = arr[i];
-        right = arr[i+1];
-        left  = arr[i-1];
-        above = arr[i-dim];
-        below = arr[i+dim];
-
-        if (is_precise_enough(precision, cell, right, left, above, below))
+        if (prec_arr[i] > max_precision)
         {
-            printf("Relaxation Complete!\n");
-            return;
+            max_precision = prec_arr[i];
         }
     }
-    return;
+    return max_precision;
+}
+
+/* Checks for the highest value in the array and returns it, the highest value
+ * is the lowest precision which is how precision is measured
+ */
+float get_current_precision(float *precision_arr, int length)
+{
+    int i;
+    float max = 0;
+    float cell_prec;
+
+    for (i = 0; i < length; ++i)
+    {
+        cell_prec = precision_arr[i];
+        // Return false if cell is not precise enough
+        if (cell_prec > max)
+        {
+            max = cell_prec;
+        }
+    }
+    return max;
+}
+
+// Uses 1D array index to check if the cell would lie on the edge of the
+// square array
+bool is_edge_index (int index, int dim)
+{
+    // Check if index is on first or last row
+    if (index < dim || index >= dim * dim - dim)
+    {
+        return true;
+    }
+
+    // Check if index is on left or right
+    int mod = (index % dim);
+    return mod == 0 || mod == dim - 1;
+}
+
+void solve (float *arr, int dim, int nthreads, float precision)
+{
+    int i;
+    int length = dim * dim;
+
+    float *precision_arr = malloc(length * sizeof(int));
+
+    /* Not precise enough yet - Relax */
+    float *temp_arr = malloc(length * sizeof(int));
+
+    // Initiliase to get inside loop
+    float current_precision = precision + 1.0;
+
+    float right, left, above, below, cell;
+
+    // Iterate
+    while (true)
+    {
+        // Calculate current precision
+        for (i = 0; i < length; ++i)
+        {
+            cell  = arr[i];
+            right = arr[i+1];
+            left  = arr[i-1];
+            above = arr[i-dim];
+            below = arr[i+dim];
+
+            // Can only calculate precision for interior cells
+            if (is_edge_index(i, dim))
+            {
+                // Nothing to do here
+            }
+            else
+            {
+                precision_arr[i] = calc_precision(cell, right, left, above, below);
+            }
+        }
+
+        current_precision = get_current_precision(precision_arr, length);
+
+        /* Check base condition - return if precision is high enough */
+        if (current_precision < precision)
+        {
+            printf("Relaxation Complete!\nPrecision: %.3f\n", current_precision);
+            return;
+        }
+
+        // Calculate new values
+        for (i = 0; i < length; ++i)
+        {
+            // Copy over edge cells (no computation needed)
+            if (is_edge_index(i, dim))
+            {
+                temp_arr[i] = arr[i];
+            }
+            // Set cell to the average of it's neighbours
+            else
+            {
+                right = arr[i+1];
+                left  = arr[i-1];
+                above = arr[i-dim];
+                below = arr[i+dim];
+
+                temp_arr[i] = (right + left + above + below) / 4;
+            }
+        }
+        // Copy over results
+        memcpy(arr, temp_arr, length * sizeof(float));
+        printf("Current precision: %.3f\nState of matrix:\n",
+               current_precision);
+        print_matrix(temp_arr);
+    }
 }
 
 int main (int argc, char *argv[])
 {
     /* Command line args: array filename, dimension, nthreads, precision */
     char *filename = "matrix";
-    int dim = 4;
+    dim = 4;
+    length = dim * dim;
     int nthreads = 8;
-    float precision = 100.2;
-    int *arr = read_array(filename, dim);
+    float precision = 7;
+    arr = read_array(filename, dim);
 
     solve(arr, dim, nthreads, precision);
 
